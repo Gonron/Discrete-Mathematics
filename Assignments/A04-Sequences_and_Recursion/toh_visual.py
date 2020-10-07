d266a94fe021b6c72c76e3dfb22061ca56aa43dc
Gonron <lundsgaard1997@hotmail.dk>
2020-10-07 08:57:56 +0200

    Finished AO4-Sequence_and_Recursion

--- Assignments/A04-Sequences_and_Recursion/toh_visual.py
+++ Assignments/A04-Sequences_and_Recursion/toh_visual.py
@@ -1,37 +1,52 @@
-"""                     
+import time
 
-   #|#        |         |    
-  ##|##       |         |
- ###|###      |         |
-####|####     |         |
-    A         B         C
-
-
-    |         |         |
-  ##|##       |         |
- ###|###      |         |
-####|####    #|#        |
-    A         B         C
-
-
-    |         |         |
-    |         |         |
- ###|###      |         |
-####|####    #|#      ##|##
-    A         B         C
-
-
-    |         |         |
-    |         |         |
- ###|###      |        #|#
-####|####     |       ##|##
-    A         B         C
+"""
+Max number of moves for:
+    3 Rings: 7
+    4 Rings: 15
+    5 Rings: 31
+    6 Rings: 63
+    7 Rings: 127
 
     ...
 
-    |         |        #|#
-    |         |       ##|##
-    |         |      ###|###
-    |         |     ####|####
-    A         B         C
+    nMaxMoves = cMaxMoves * 2 + 1 // 15 * 2 + 1 = 31
 """
+
+
+
+class Hanoi():
+    def __init__(self, n):
+        self.n = n
+        self. moves = 0
+        self.A = list(range(n, 0, -1)) # Populate pole A with numbers n to 1
+        self.B = list()
+        self.C = list()
+        return
+
+    def __str__(self):
+        return f'Move: {self.moves}\n\tA: {self.A}\n\tB: {self.B}\n\tC: {self.C}'
+
+    def solve(self, rings, origin, odd, target):
+        if rings == 1:
+            move = origin.pop() # Remove ring from origin pole
+            target.append(move) # Appends ring to target pole
+            self.moves += 1 # Counts up moves
+            print(tower)
+        else:
+            self.solve(rings-1, origin, target, odd)
+            move = origin.pop() 
+            target.append(move) 
+            self.moves += 1
+            print(tower)
+            self.solve(rings-1, odd, origin, target)
+        return
+
+if __name__ == "__main__":
+    n = 4
+    tower = Hanoi(n)
+    print(tower)
+    tower.solve(tower.n, tower.A, tower.B, tower.C)
+
+
+    
